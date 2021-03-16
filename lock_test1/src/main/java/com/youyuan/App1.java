b23ac2b3b594faeb465b1a54f460d691a9de094a
zhangy <zhangyu2046196@163.com>
2021-03-16 17:47:30 +0800

    springboot+redis实现分布式锁

---
+++ lock_test1/src/main/java/com/youyuan/App1.java
@@ -0,0 +1,21 @@
+package com.youyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 类名称：App <br>
+ * 类描述： 主启动类 <br>
+ *
+ * @author zhangyu
+ * @version 1.0.0
+ * @date 创建时间：2021/3/16 11:26<br>
+ */
+@SpringBootApplication
+public class App1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App1.class, args);
+    }
+
+}
